e37a58c436527c0a4317dc1ce929cc0683770044
Mateo Wallace <mateo.t.wallace@gmail.com>
2022-10-15 12:17:12 -0400

    Send apiSearch function to 2 functions. 1 to find news and 1 to print relevant data to page

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,8 +1,5 @@
-
-var gameSearch = document.getElementById("searchBar");
-var searchButton = document.getElementById("searchButton");
+// Global Variables
 const apiKey = "7ca66c0b57msh5f0900adbde527ap12f1d6jsn65ee2844dd63"
-var searchForm = document.getElementById("searchForm")
 const apiUrl = 'https://steam2.p.rapidapi.com/search/'
 const options = {
 	method: 'GET',
@@ -12,54 +9,44 @@ const options = {
 	}
 };
 
+// DOM Element Variables
+var gameSearch = document.getElementById("searchBar");
+var searchButton = document.getElementById("searchButton");
+var searchForm = document.getElementById("searchForm")
 
+// Function to pull AppId data from Steam API
 function apiSearch() {
-    try {
-    fetch(apiUrl+ gameSearch.value +'/page/1', options)
-	.then(response => response.json())
-	.then((data) => (data[0].title))
-	.catch(err => console.error(err));
-    let searchData = data;
-    searchData.map(function(searchData){
-        let li = document.createElement('li');
-        let title = document.createElement('h2');
-    
-        title.innerHTML = `${data.title}`;
-        li.appendChild(title);
-        console.log()
-    });
-    }
-  finally {
-     console.log(data);
-}
-}
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    function handleSearchFormSubmit(e){
-        e.preventDefault();
-        console.log(e.target)
-   var rawSearch = gameSearch.value.trim();
-   var search = rawSearch.charAt(0).toUpperCase() + rawSearch.slice(1).toLowerCase();
-    console.log(search);
-    apiSearch(search);
-    gameSearch.value = '';
+	fetch(apiUrl + gameSearch.value + '/page/1', options)
+		.then(response => response.json())
+		.then(function (data) {
+			var appIdData = data[0].appId
+			steamNews (appIdData)
+			renderSearchData (data)
+		})
+		.catch(err => console.error(err));
 }
 
-    searchButton.addEventListener("click", apiSearch);
-   searchForm.addEventListener('submit', handleSearchFormSubmit);
+// Call fetch api for steam news and pull down data
+function steamNews (appIdData) {
+	console.log(appIdData)
+}
+
+// Render relevant game data to the page, using the data from steam search api
+function renderSearchData (data) {
+	console.log(data)
+	var imgUrlData
+	var priceData
+	var titleData
+	var steamLinkData
+}
+
+function handleSearchFormSubmit(e) {
+	e.preventDefault();
+	var rawSearch = gameSearch.value.trim();
+	var search = rawSearch.charAt(0).toUpperCase() + rawSearch.slice(1).toLowerCase();
+	apiSearch(search);
+	gameSearch.value = '';
+}
+
+searchButton.addEventListener("click", handleSearchFormSubmit);
+searchForm.addEventListener('submit', handleSearchFormSubmit);
