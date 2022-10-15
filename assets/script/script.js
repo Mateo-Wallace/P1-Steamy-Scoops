6a1eefcf3ad3ad6418f7b47b49c456b6503b15e7
Andrew PG <andrewpgibbs71@gmail.com>
2022-10-15 11:34:57 -0400

    experimental js

--- assets/script/script.js
+++ assets/script/script.js
@@ -3,6 +3,7 @@ var gameSearch = document.getElementById("searchBar");
 var searchButton = document.getElementById("searchButton");
 const apiKey = "7ca66c0b57msh5f0900adbde527ap12f1d6jsn65ee2844dd63"
 var searchForm = document.getElementById("searchForm")
+const apiUrl = 'https://steam2.p.rapidapi.com/search/'
 const options = {
 	method: 'GET',
 	headers: {
@@ -13,13 +14,39 @@ const options = {
 
 
 function apiSearch() {
-    fetch('https://steam2.p.rapidapi.com/search/'+ gameSearch.value +'/page/1', options)
+    try {
+    fetch(apiUrl+ gameSearch.value +'/page/1', options)
 	.then(response => response.json())
-	.then(response => console.log(response))
+	.then((data) => (data[0].title))
 	.catch(err => console.error(err));
+    let searchData = data;
+    searchData.map(function(searchData){
+        let li = document.createElement('li');
+        let title = document.createElement('h2');
     
+        title.innerHTML = `${data.title}`;
+        li.appendChild(title);
+        console.log()
+    });
+    }
+  finally {
+     console.log(data);
+}
 }
     
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -34,5 +61,5 @@ function apiSearch() {
     gameSearch.value = '';
 }
 
-
-   searchForm.addEventListener('submit', handleSearchFormSubmit)
+    searchButton.addEventListener("click", apiSearch);
+   searchForm.addEventListener('submit', handleSearchFormSubmit);
